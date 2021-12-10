d9f063f3cc88ba83d3407dc798f0112f714c97e6
DiegoVP66 <dvpdev33@gmail.com>
2021-12-10 17:14:36 -0300

    Entities City Event adjustments, Role and User created

--- src/main/java/com/devsuperior/bds04/entities/Role.java
+++ src/main/java/com/devsuperior/bds04/entities/Role.java
@@ -1,36 +1,31 @@
 package com.devsuperior.bds04.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_city")
-public class City implements Serializable {
+@Table(name = "tb_role")
+public class Role implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
+	private String authority;
 
-	@OneToMany(mappedBy = "city")
-	private List<Event> events = new ArrayList<>();
-
-	public City() {
+	public Role() {
 	}
 
-	public City(Long id, String name) {
+	public Role(Long id, String authority) {
 		this.id = id;
-		this.name = name;
+		this.authority = authority;
 	}
 
 	public Long getId() {
@@ -41,16 +36,12 @@ public class City implements Serializable {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getAuthority() {
+		return authority;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<Event> getEvents() {
-		return events;
+	public void setAuthority(String authority) {
+		this.authority = authority;
 	}
 
 	@Override
@@ -66,7 +57,7 @@ public class City implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		City other = (City) obj;
+		Role other = (Role) obj;
 		return Objects.equals(id, other.id);
 	}
 
